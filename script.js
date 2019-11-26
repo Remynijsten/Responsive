f3d549f8658fe63d9684e226a59f3531cc77a750
Remy Nijsten <99012252@mydavinci.nl>
2019-11-26 11:32:13 +0100

    Updated Responsive

--- script.js
+++ script.js
@@ -26,14 +26,17 @@ $(".navBTN").click(function(){
 function slide(){
 	if (index == 1){
 		$(".slideIMG").attr("src","img/vector2.jpg");
+		$(".imgTEXT").html("<span>Veiligheid<br></span>Staat centraal bij ons!");
 		index++;
 	}
 	else if (index == 2){
 		$(".slideIMG").attr("src","img/vector3.jpg");
+		$(".imgTEXT").html("<span>Je bent bij het goede adres!<br></span>Wanneer het gaat om vertrouwen.");
 		index++
 	}
 	else if (index == 3){
 		$(".slideIMG").attr("src","img/vector1.jpg");
+		$(".imgTEXT").html("<span>Een Rijschool<br></span>Die meer dan alleen een rijschool is");
 		index = 1;
 	}
 }
